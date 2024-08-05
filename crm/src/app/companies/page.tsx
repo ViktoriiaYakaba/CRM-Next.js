537359f1db4ee5b94551792b3c6b3f17c058ac6d
ViktoriiaYakaba <yakaba@bk.ru>
2024-08-05 18:42:50 +0200

    add page.tsx

--- crm/src/app/companies/page.tsx
+++ crm/src/app/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "../components/header";
 import SearchInput from "../components/search-input";
-import Toolbar from "../components/toolbar";
+import ToolBar from '@/app/components/toolbar'
 import AddCompanyButton from "../components/add-company-button";
 import CompanyTable from "../components/company-table";
 import CompanyRow from "../components/company-row";
-
+import { Status } from '@/app/components/status-label';
 export interface PageProps{
 
 }
@@ -17,7 +17,13 @@ export default function ({ }: PageProps) {
             <SearchInput />
         </ToolBar>
         <CompanyTable>
-<CompanyRow id={1} category={'Products'} company={'Costco'} status={Status.Pending} promotion={true} country={'USA'} joineDate={'02.19.2013'}/>
+            <CompanyRow id={1}
+                category={'Products'}
+                company={'Costco'}
+                status={Status.Pending}
+                promotion={true}
+                country={'USA'}
+                joineDate={'02.19.2013'} />
         </CompanyTable>
     </>
-}
+};
